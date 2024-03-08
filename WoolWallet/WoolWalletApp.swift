cc9fdbdd8572190d215f1fb58283b647a36c3429
Logan Werner <logan.austin.werner@gmail.com>
2024-03-08 11:17:40 -0500

    Initial Commit

---
+++ WoolWallet/WoolWalletApp.swift
@@ -0,0 +1,17 @@
+//
+//  WoolWalletApp.swift
+//  WoolWallet
+//
+//  Created by Mac on 3/8/24.
+//
+
+import SwiftUI
+
+@main
+struct WoolWalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
